150060ce4b83928e71d4aeadb94a4f1856cf38e1
Joseph Gabito <codehaiku.io@gmail.com>
2018-06-19 05:58:41 +0800

    new update

--- src/Enqueue.php
+++ src/Enqueue.php
@@ -14,6 +14,8 @@ final class Enqueue {
 	{
 		wp_register_script( 'frontend-filemanager', plugins_url() . '/front-end-file-manager/public/front-end-file-manager.js', array('jquery','plupload'), 1.0, true );
 
+		wp_register_style( 'frontend-filemanager-style', plugins_url() . '/front-end-file-manager/public/css/front-end-file-manager.css', array() );
+
 		wp_localize_script( 'frontend-filemanager', 'frontend_filemanager', array(
 			'rest_url' => rest_url('frontend-filemanager/v1/'),
 			'nonce' => wp_create_nonce( 'wp_rest' ),
