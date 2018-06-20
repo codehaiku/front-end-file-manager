07a95c35cf4bc00bea97ae0612898d7ddba3b30d
Joseph Gabito <codehaiku.io@gmail.com>
2018-06-20 10:51:08 +0800

    new update

--- src/templates/single-view.php
+++ src/templates/single-view.php
@@ -1,16 +1,30 @@
 <a href="#list">&larr; Back to list</a>
 
-<form>
+<form id="fefm-update-file" method="post" multipart="form/data" 
+action="<?php echo esc_url( rest_url('frontend-filemanager/v1/update') ); ?>">
+
 	<div class="form-group">
-		<label for="fefm-form-file-label"><?php esc_html_e('File Label', 'front-end-file-manager'); ?></label>
-		<input type="text" class="form-control" id="fefm-form-file-label" value="<%=file_label%>">
+		<label for="fefm-form-file-label"><?php esc_html_e('Label', 'front-end-file-manager'); ?></label>
+		<input type="text" class="form-control" id="fefm-form-file-label" name="file_label" value="<%=file_label%>">
+		<input type="hidden" name="id" value="<%=id%>">
 	</div>
 
 	<div class="form-group">
-		<label for="exampleInputPassword1"><?php esc_html_e('File Description', 'front-end-file-manager'); ?></label>
-		<textarea class="form-control"><%=file_description%></textarea>
+		<label for="file_description"><?php esc_html_e('Description', 'front-end-file-manager'); ?></label>
+		<textarea id="file_description" name="file_description" class="form-control"><%=file_description%></textarea>
 		<p class="help-block">
 			<?php esc_html_e('Describe what this file is all about', 'front-end-file-manager'); ?>
+		</p>
+	</div>
+	<div class="form-group">
+		<label for="file_sharing_type"><?php esc_html_e('Sharing Type', 'front-end-file-manager'); ?></label>
+		<select class="form-control" name="file_sharing_type">
+
+			<option <%=('private' === file_sharing_type) ? 'selected': ''%> value="private"><?php esc_html_e('Private', 'front-end-file-manager'); ?></option>
+			<option <%=('public' === file_sharing_type) ? 'selected': ''%> value="public"><?php esc_html_e('Public', 'front-end-file-manager'); ?></option>
+		</select>
+		<p class="help-block">
+			<?php esc_html_e('Select the type of privacy for this file', 'front-end-file-manager'); ?>
 		</p>
 	</div>
 
