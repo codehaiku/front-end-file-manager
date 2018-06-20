faa4bd171e2b21b9a7da0877ab8419cff81ca23b
Joseph Gabito <codehaiku.io@gmail.com>
2018-06-20 10:51:08 +0800

    new update

--- src/templates/index-loop.php
+++ src/templates/index-loop.php
@@ -1,5 +1,4 @@
 <div id="fefm-wrap">
-
 	<ul id="fefm-controls">
 		<li><a class="fefm-controls-btn" id="fefm-controls-btn-uploaded" href="#">Upload</a></li>
 		<li><a class="fefm-controls-btn fefm-controls-btn-link" href="#">New Folder</a></li>
@@ -21,28 +20,5 @@
 	<?php include_once trailingslashit( FEFM_DIR ) . 'src/templates/single-view.php'; ?>
 </script>
 <script id="fefm-single-file-template" type="text/template">
-	<li class="file-item" id="fefm-file-<%=id%>">
-		<div class="file-item-column file-thumb">
-			<img width="24" src="<%=file_icon%>" />
-		</div>
-		<div class="file-item-column file-label">
-			<a href="#file/<%=id%>" title="<%=file_label%>">
-				<%=file_label%>
-			</a>
-		</div>
-		<div class="file-item-column file-date-updated"><%=date_updated%></div>
-		<div class="file-item-column file-sharing-type"><%=file_sharing_type%></div>
-		<div class="file-item-column file-actions">
-			<a href="#" class="file-item-column-file-actions-dropdown">
-				<span class="fefm-action-label">Select Actions</span>
-			</a>
-			<ul class="file-item-column-file-actions-dropdown-ul inactive">
-				<li><a href="#">Edit</a></li>
-				<li><a href="#">Copy</a></li>
-				<li><a href="#">Share</a></li>
-				<li><a data-file-id="<%=id%>" class="fefm-item-file-trash" href="#">Trash</a></li>
-				<li><a class="fefm-toolbar-close" href="#">Close Toolbar</a></li>
-			</ul>
-		</div>
-	</li>
+	<?php include_once trailingslashit( FEFM_DIR ) . 'src/templates/list.php'; ?>
 </script>
